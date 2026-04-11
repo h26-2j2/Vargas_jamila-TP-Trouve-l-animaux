cc29beadd0ac7cfdc8729ad7759987d8898c8479
Jamila-V <e2134393@cmaisonneuve.qc.ca>
2026-04-11 11:57:57 -0400

    son+changementscene

--- Assets/Script/GestionJeu.cs
+++ Assets/Script/GestionJeu.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;  
+using UnityEngine.InputSystem;
 
 public class GestionJeu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //Generer un Scene Aléatoire   
+    public float SceneAleatoire = 0f;
+
     void Start()
     {
-        
+        //Generer le nombre au Debut du Jeu
+        SceneAleatoire = Random.Range(0f, 100f);
+        //Garder le script entre les Scènes
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //Demarrer le Jeu avec la touche Espace
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            DemarrerJeu();
+        }
     }
+
+    //Activer avec le bouton Demarrer
+    public void DemarrerJeu()
+    {
+        //Scene aleatoire
+        if (SceneAleatoire < 50f)
+        {
+            SceneManager.LoadScene("niv-1-chien");
+        }
+        else if (SceneAleatoire >= 50f)
+        {
+            SceneManager.LoadScene("niv-1-chat");
+        }
+    }
+    //public void Recommencer()
+    //{
+    //    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    ////}
 }
